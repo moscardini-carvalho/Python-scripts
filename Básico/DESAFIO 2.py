85f3c1cc37303b3bdcbecc654d81d6b9d0c2eb5d
João Pedro <162808430+moscardini-carvalho@users.noreply.github.com>
2024-05-23 15:12:16 -0300

    Add files via upload

---
+++ Básico/DESAFIO 2.py
@@ -0,0 +1,5 @@
+dia = int(input('Que dia você nasceu?: '))
+mes = input('Que mês você nasceu?: ')
+ano = int(input('Que ano você nasceu?: '))
+
+print(f'Você no dia', dia,'de', mes,'de', ano,'. Está correto?')
---
+++ Básico/ex000.py
@@ -0,0 +1,4 @@
+nome = input('Qual o seu nome?: ')
+idade = int(input('Qual a sua idade?: '))
+peso = float(input('Qual o seu peso?: '))
+print(nome, idade, peso)
