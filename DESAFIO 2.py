d98f24fa26e3144978f5b0d4472b0d855af8c382
João Pedro <162808430+moscardini-carvalho@users.noreply.github.com>
2024-05-23 15:12:51 -0300

    Delete DESAFIO 2.py

--- DESAFIO 2.py
+++
@@ -1,5 +0,0 @@
-dia = int(input('Que dia você nasceu?: '))
-mes = input('Que mês você nasceu?: ')
-ano = int(input('Que ano você nasceu?: '))
-
-print(f'Você no dia', dia,'de', mes,'de', ano,'. Está correto?')
